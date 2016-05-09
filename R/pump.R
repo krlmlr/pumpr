0a3740d640dae15bde1cdcbc1b2b8f7f3e16b058
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-05-09 14:04:19 +0200

    pump()

--- R/pump.R
+++ R/pump.R
@@ -4,6 +4,16 @@ Pump <- R6::R6Class(
     initialize = function(get_chunk, has_completed) {
       private$get_chunk = get_chunk
       private$has_completed = has_completed
+    },
+
+    pump = function() {
+      stack <- rstackdeque::rstack()
+
+      while (!private$has_completed()) {
+        stack <- rstackdeque::insert_top(stack, private$get_chunk())
+      }
+
+      as.list(stack)
     }
   ),
 
--- tests/testthat/test-pump.R
+++ tests/testthat/test-pump.R
@@ -1,6 +1,19 @@
 context("pump")
 
+set.seed(20160509L)
+
 test_that("can construct", {
   random_pump <- Pump$new(get_chunk = function() runif(10000),
                           has_completed = function() runif(1) < 1e-3)
+})
+
+test_that("can pump", {
+  count <- 0L
+  random_pump <- Pump$new(get_chunk = function() runif(10000),
+                          has_completed = function() { count <<- count + 1L; count > 5L})
+
+  pump_result <- random_pump$pump()
+
+  expect_is(pump_result, "list")
+  expect_equal(length(pump_result), 5)
 })
